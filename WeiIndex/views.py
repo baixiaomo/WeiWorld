4b945bc542b2724b77dde3e1affab44f0bd9337e
魏飞 <feiwei@creditease.cn>
2017-06-05 15:55:53 +0800

    Initial commit

---
+++ WeiIndex/views.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+from django.shortcuts import render,render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return render_to_response('index.html',context_instance=RequestContext(request))
