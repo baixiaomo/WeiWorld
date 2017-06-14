968824d643b2bbaed8abb65625d8f4d5791b68d0
魏飞 <feiwei@creditease.cn>
2017-06-14 18:58:08 +0800

    查询模板

--- WeiIndex/views.py
+++ WeiIndex/views.py
@@ -1,8 +1,14 @@
 #coding:utf-8
 from django.shortcuts import render,render_to_response
 from django.template import RequestContext
+from WeiIndex.models import WeiIndexModel
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render_to_response('index.html',context_instance=RequestContext(request))
+
+    queryset = WeiIndexModel.objects.filter(state=True)#查询生效模板
+    print(queryset)
+    print([p.name for p in queryset])
+
+    return render_to_response('index.html',context={'indexModel':queryset},context_instance=RequestContext(request))
